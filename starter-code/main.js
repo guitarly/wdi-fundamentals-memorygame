a3e4568a0805470e3af506a8ba086d07428c04de
Guitarly <guitarly@gmail.com>
2017-02-21 18:07:44 -0500

    modified games functions

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,12 @@
 console.log("JS file is connected to HTML! Woo!")
+
+var cardOne = 'Queen' ;
+var cardTwo = 'Queen' ;
+var cardThree = 'King' ;
+var cardFour = 'King' ;
+
+// if(cardOne === cardTwo || cardThree === cardFour) {
+//   alert('You found a match')
+// } else {
+//   alert('Sorry, try again.')
+// }
