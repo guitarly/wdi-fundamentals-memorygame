bcadb363e78d1403f18cdd06ef263d650b82b9c8
Guitarly <guitarly@gmail.com>
2017-02-22 15:13:41 -0500

    add more functions for Cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,25 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = 'Queen' ;
-var cardTwo = 'Queen' ;
-var cardThree = 'King' ;
-var cardFour = 'King' ;
-
-if(cardOne === cardTwo || cardThree === cardFour) {
-  alert('You found a match')
-} else {
-  alert('Sorry, try again.')
+// var cardOne = 'Queen' ;
+// var cardTwo = 'Queen' ;
+// var cardThree = 'King' ;
+// var cardFour = 'King' ;
+var cards = ['queen', 'queen', 'king', 'king'];
+var gameBoard = document.getElementById('game-board');
+console.log(gameBoard);
+function createCards() {
+  //loop - add four card elements
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('div');
+    cardElement.className = "card";
+    gameBoard.appendChild(cardElement);
+  }
 }
+
+createCards();
+
+// if(cardOne === cardTwo || cardThree === cardFour) {
+//   alert('You found a match')
+// } else {
+//   alert('Sorry, try again.')
+// }
