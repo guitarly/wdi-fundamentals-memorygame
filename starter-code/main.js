789a5c7a79f5d40df8bfaaeea2f413b9bdffe476
Guitarly <guitarly@gmail.com>
2017-02-22 21:53:25 -0500

    finished memory cards games

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,25 +1,70 @@
 console.log("JS file is connected to HTML! Woo!")
 
-// var cardOne = 'Queen' ;
-// var cardTwo = 'Queen' ;
-// var cardThree = 'King' ;
-// var cardFour = 'King' ;
 var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [] ;
+
 var gameBoard = document.getElementById('game-board');
-console.log(gameBoard);
-function createCards() {
+
+function createBoard() {
+
+shuffle(cards);
+
   //loop - add four card elements
   for (var i = 0; i < cards.length; i++) {
     var cardElement = document.createElement('div');
     cardElement.className = "card";
+    cardElement.setAttribute('data-card', cards[i]);
+    cardElement.addEventListener('click', isTwoCards);
+
     gameBoard.appendChild(cardElement);
+
   }
 }
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+  if (this.getAttribute('data-card') === 'king') {
+    //this.innerHTML = cardsInPlay.push(this.getAttribute('data-card'));
+    this.innerHTML ="<img src='http://i.imgur.com/bnuv5Im.png'>"; // king
+  } else {
+    //this.innerHTML = cardsInPlay.push(this.getAttribute('data-card'));
+    this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>"; //queen
+  }
 
-createCards();
+  if (cardsInPlay.length > 1) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+};
 
-// if(cardOne === cardTwo || cardThree === cardFour) {
-//   alert('You found a match')
-// } else {
-//   alert('Sorry, try again.')
-// }
+function isMatch(cards) {
+  // alert winning message
+  for (var i = 0; i < cards.length; i++) {
+    console.log(cards[i]);
+  }
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+} ;
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+createBoard();
